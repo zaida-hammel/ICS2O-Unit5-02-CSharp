cb467b5f5a7ef8d6701fbcf99414374f2f86a85d
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-27 12:57:30 -0400

    edited whitespace

--- main.cs
+++ main.cs
@@ -7,15 +7,17 @@ using System;
 
 class Program
 {
-    public static void Main(string[] args) {
+    public static void Main(string[] args)
+    {
         int userInteger;
         Console.WriteLine("Write any number (positive or negative):");
         userInteger = int.Parse(Console.ReadLine());
-        if (userInteger > -1){
+        if (userInteger > -1)
+        {
             userInteger = Convert.ToInt32(Console.Read());
             Console.WriteLine("The integer you input is positive.");
             Console.ReadLine();
-        }else{
+        } else {
             Console.WriteLine("The integer you input is a negative.");
         }
     }
